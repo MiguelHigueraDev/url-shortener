66dc1dbe9105a147cb90059598d52e52a59a75dd
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-02 15:40:40 -0400

    chore: prettier

--- frontend/src/app/services/shortener-service.service.ts
+++ frontend/src/app/services/shortener-service.service.ts
@@ -3,21 +3,24 @@ import { environment } from '../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShortenerService {
-
   private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
   shortenUrl(url: string) {
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     });
 
-    return this.http.post(`${this.baseUrl}/shorten`, {
-      "originalUrl": url
-    }, { headers});
+    return this.http.post(
+      `${this.baseUrl}/shorten`,
+      {
+        originalUrl: url,
+      },
+      { headers }
+    );
   }
 }
