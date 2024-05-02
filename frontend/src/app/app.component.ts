b1b90caad2ab7fb25218d3440c26a66a3b2cb7f8
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-02 14:50:04 -0400

    feat(frontend): add header

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ShortenerService } from './services/shortener-service.service';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
