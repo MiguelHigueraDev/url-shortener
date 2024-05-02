ce8095f42b5c84451e1f57fe23909b21f4a09b3a
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-02 15:19:27 -0400

    fix(backend, cors): update allowed origins

--- backend/src/main/java/dev/miguelhiguera/urlshortener/configs/CorsConfig.java
+++ backend/src/main/java/dev/miguelhiguera/urlshortener/configs/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4230", "https://misft.me")
+                        .allowedOrigins("http://localhost:4200", "https://misft.me")
                         .allowedMethods("GET", "POST", "OPTIONS", "DELETE");
             }
         };
