30d42d8a9fd1e039dedcbe4807a00c36a64e6a36
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-02 14:22:51 -0400

    feat: make simple form to test API endpoint

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShortenerService } from './services/shortener-service.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'url-shortener';
+
+  constructor(private shortenerService: ShortenerService) {}
+
+  originalUrl: string = '';
+
+  shortenUrl() {
+    this.shortenerService.shortenUrl(this.originalUrl).subscribe((response: any) => {
+      console.log(response);
+    });
+  }
 }
