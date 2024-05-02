7024340db1f4d96f8cf7669beed5f35fcc3720eb
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-02 13:48:56 -0400

    fix: add unique constraint to urls

--- backend/src/main/java/dev/miguelhiguera/urlshortener/entities/Url.java
+++ backend/src/main/java/dev/miguelhiguera/urlshortener/entities/Url.java
@@ -19,10 +19,10 @@ public class Url {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String originalUrl;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String shortUrl;
 
     @Column(nullable = false)
