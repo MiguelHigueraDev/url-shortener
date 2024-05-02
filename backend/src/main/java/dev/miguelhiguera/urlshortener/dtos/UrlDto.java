581ce4656592662332ac8387b47048ebf542a62f
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-02 15:47:44 -0400

    fix(backend): improve url validation

--- backend/src/main/java/dev/miguelhiguera/urlshortener/dtos/UrlDto.java
+++ backend/src/main/java/dev/miguelhiguera/urlshortener/dtos/UrlDto.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class UrlDto {
 
     @NotBlank
-    @Pattern(regexp = "^(http|https)://.*$", message = "Url must start with http:// o https://")
+    @Pattern(regexp = "^(https://[a-zA-Z0-9\\-.]+\\.[a-zA-Z]{2,}(\\.[a-zA-Z]{2,})?).*$", message = "Url must start with http:// or https:// and contain a valid domain")
     @Size(min = 10, max = 255, message = "Url must be between 10 and 255 characters")
     private String originalUrl;
 }
