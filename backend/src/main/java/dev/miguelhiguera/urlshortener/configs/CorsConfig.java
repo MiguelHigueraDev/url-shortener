e98165a70dea0f810497126c67c1e48d6d8f7bfa
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-03 12:03:51 -0400

    fix: update cors config to add new domain

--- backend/src/main/java/dev/miguelhiguera/urlshortener/configs/CorsConfig.java
+++ backend/src/main/java/dev/miguelhiguera/urlshortener/configs/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://misft.me")
+                        .allowedOrigins("http://localhost:4200", "https://misft.me", "https://create.misft.me")
                         .allowedMethods("GET", "POST", "OPTIONS", "DELETE");
             }
         };
