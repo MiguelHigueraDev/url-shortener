9573715ba6d6d180ca12e801fec364881075194e
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-02 21:15:51 -0400

    fix: use environment variables as shortener properties

--- backend/src/main/java/dev/miguelhiguera/urlshortener/configs/ShortenerProperties.java
+++ backend/src/main/java/dev/miguelhiguera/urlshortener/configs/ShortenerProperties.java
@@ -1,17 +1,24 @@
 package dev.miguelhiguera.urlshortener.configs;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Data
 @Component
-@ConfigurationProperties(prefix = "shortener")
 public class ShortenerProperties {
 
+    @Value("${SHORTENER_BASE_URL}")
     private String baseUrl;
+
+    @Value("${SHORTENER_SHORT_URL_MIN_LENGTH}")
     private int shortUrlMinLength;
+
+    @Value("${SHORTENER_SHORT_URL_MAX_LENGTH}")
     private int shortUrlMaxLength;
+
+    @Value("${SHORTENER_CHARSET}")
     private String charset;
 
 }
