41c6c2fd434df2a162577a679640a7504952555d
Miguel Higuera <133175356+MiguelHigueraDev@users.noreply.github.com>
2024-05-05 04:54:11 -0400

    feat: add footer with generated urls

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ShortenerFormComponent } from './components/shortener-form/shortener-form.component';
 import { HistoryComponent } from './components/history/history.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ShortenerFormComponent, HistoryComponent],
+  imports: [RouterOutlet, HeaderComponent, ShortenerFormComponent, HistoryComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
